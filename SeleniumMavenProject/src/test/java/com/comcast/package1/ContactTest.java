74a596cfa392b5a5a62481f7f911cef042d06b08
taswika143 <manusasi@DESKTOP-2U9NDB1.mshome.net>
2025-03-17 17:53:30 +0530

    sample framework

---
+++ SeleniumMavenProject/src/test/java/com/comcast/package1/ContactTest.java
@@ -0,0 +1,17 @@
+package com.comcast.package1;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+
+	@Test
+	public void createContactTest() {
+		System.out.println("create contact test");
+	}
+	
+	
+	@Test
+	public void modifyContactTest() {
+		System.out.println("modify contact test");
+	}
+}
---
+++ SeleniumMavenProject/src/test/java/com/comcast/package2/OrgTest.java
@@ -0,0 +1,17 @@
+package com.comcast.package2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+
+	@Test
+	public void createOrgTest() {
+		System.out.println("create org test");
+	}
+	
+	
+	@Test
+	public void modifyOrgTest() {
+		System.out.println("modify org test");
+	}
+}
