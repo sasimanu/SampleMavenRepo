2271b8fc19a743189c896ba856d8a741ca8385e7
taswika143 <manusasi@DESKTOP-2U9NDB1.mshome.net>
2025-03-18 11:15:12 +0530

    contact test

--- SeleniumMavenProject/src/test/java/com/comcast/package1/ContactTest.java
+++ SeleniumMavenProject/src/test/java/com/comcast/package1/ContactTest.java
@@ -6,6 +6,15 @@ public class ContactTest {
 
 	@Test
 	public void createContactTest() {
+		String URL= System.getProperty("url","http://testenv.com");
+		String BROWSER= System.getProperty("browser","chrome");
+		String USERNAME= System.getProperty("username","admin");
+		String PASSWORD= System.getProperty("password","admin");
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
 		System.out.println("create contact test");
 	}
 	
